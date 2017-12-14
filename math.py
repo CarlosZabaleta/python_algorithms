05fca09434f1a98019129397e0e0c412bcadc549
Carlos Zabaleta <carlosu@debian>
2017-12-14 18:31:53 -0500

    Add string and math files

---
+++ math.py
@@ -0,0 +1,13 @@
+# type of numbers
+
+a = 1
+b = int("1")
+print(a + b)
+{
+
+}
+# x = input("insert number: ")
+# print (x)
+
+for num in [1, 2, 4, 5, 6]:
+    print(num)
---
+++ strings.py
@@ -0,0 +1,10 @@
+# Variable
+import time
+import sys
+
+for i in range(5):
+    print("hello world")
+    time.sleep(1)
+
+a = "hello world"
+print sys.executable
